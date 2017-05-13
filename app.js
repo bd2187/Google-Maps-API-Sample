9460aabd6d7bf089463c116df36e561ed66c7de2
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-05-12 23:56:44 -0700

    Display map. Add marker. Add event listener to marker. When marker is clicked display infowindow.

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
+
 // This is the cb function that will be called once the API request is successful
 function initMap() {
-   var uluru = {lat: -25.363, lng: 131.044};
+  var sfCoords = {lat: 37.773972, lng: -122.431297};
 
-   // Construct new Google Maps object.
-   var map = new google.maps.Map(document.getElementById('map'), {
-     // Add properties to the map including the center and zoom level
+  // Construct new Google Maps object.
+  var map = new google.maps.Map(document.getElementById('map'), {
+   // Add properties to the map including the center and zoom level
+   center: sfCoords, // center property tells the API where to center the map.
+   zoom: 12 // zoom property specifies the zoom level for the map
+  });
 
-     // zoom property specifies the zoom level for the map
-     zoom: 4,
+  // Put marker on map. Position property sets position of marker
+  var marker = new google.maps.Marker({
+   position: sfCoords,
+   map: map,
+   title: 'First marker'
+  });
 
-     // center property tells the API where to center the map.
-     center: uluru
-   });
+  var infowindow = new google.maps.InfoWindow({
+    content: 'SF'
+  });
 
-   // Put marker on map. Position property sets position of marker
-   var marker = new google.maps.Marker({
-     position: uluru,
-     map: map
-   });
+  marker.addListener('click', function(){
+    infowindow.open(map, marker);
+  });
 
-   console.log(marker)
-   console.log(google)
+
 }
