fe1d696e3ea3c18a282f3532e2c9509c0bfe9b6e
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-05-15 14:11:08 -0700

    Add places library.

--- app.js
+++ app.js
@@ -27,32 +27,80 @@
 
 
 // === With Geolocation:
+// function initMap() {
+//   var map = new google.maps.Map(document.getElementById('map'),{
+//     center: {lat: -34.397, lng: 150.644},
+//     zoom: 12
+//   });
+//
+//   function success(position) {
+//     var pos = { lat: position.coords.latitude, lng: position.coords.longitude };
+//     var marker = new google.maps.Marker({
+//       position: pos,
+//       map: map
+//     });
+//
+//     map.setCenter(pos);
+//   }
+//
+//   function fail(browserHasGeolocation, infoWindow, pos) {
+//     infowindow.setPosition(pos);
+//     infowindow.setContent(browserHasGeolocation ?
+//                           'Error: The Geolocation service failed.' :
+//                           'Error: Your browser doesn\'t support geolocation.');
+//     infowindow.open(map);
+//   }
+//
+//   // check if geolocation is available in user's browser
+//   return navigator.geolocation
+//     ? navigator.geolocation.getCurrentPosition(success, fail)
+//     : fail(false, infowindow, map.getCenter());
+// }
+
+
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+
+// === With Places Library:
+var map;
+var infowindow;
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'),{
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 12
+  var pyrmont = {lat: -33.867, lng: 151.195};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont,
+    zoom: 15
   });
 
-  function success(position) {
-    var pos = { lat: position.coords.latitude, lng: position.coords.longitude };
-    var marker = new google.maps.Marker({
-      position: pos,
-      map: map
-    });
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: pyrmont,
+    radius: 500,
+    type: ['store']
+  }, callback);
+}
 
-    map.setCenter(pos);
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
   }
+}
 
-  function fail(browserHasGeolocation, infoWindow, pos) {
-    infowindow.setPosition(pos);
-    infowindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infowindow.open(map);
-  }
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
 
-  // check if geolocation is available in user's browser
-  return navigator.geolocation
-    ? navigator.geolocation.getCurrentPosition(success, fail)
-    : fail(false, infowindow, map.getCenter());
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
